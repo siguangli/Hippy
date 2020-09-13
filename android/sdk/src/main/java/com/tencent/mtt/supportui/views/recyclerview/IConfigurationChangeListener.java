fcf21917cbc7f676c1986c13295090af9465b51b
maxli <maxli@tencent.com>
2020-09-14 00:21:57 +0800

    fix(android): add support ui for debug

---
+++ android/sdk/src/main/java/com/tencent/mtt/supportui/views/recyclerview/IConfigurationChangeListener.java
@@ -0,0 +1,11 @@
+package com.tencent.mtt.supportui.views.recyclerview;
+
+import android.content.res.Configuration;
+
+/**
+ * Created by niuniuyang on 2020-07-21. Description
+ */
+public interface IConfigurationChangeListener {
+
+    void onConfigurationChanged(Configuration newConfig);
+}
