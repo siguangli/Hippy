862aee11921128063d2f1f23c76d063dafe2f26e
etkmao <etkmao@tecent.com>
2023-08-07 16:03:02 +0800

    fix(perf): support fps calculation for js

--- framework/ios/base/bridge/HippyBridge.h
+++ framework/ios/base/bridge/HippyBridge.h
@@ -181,8 +181,6 @@ HP_EXTERN NSString *HippyBridgeModuleNameForClass(Class bridgeModuleClass);
                  args:(NSArray *)args
            completion:(dispatch_block_t _Nullable)completion;
 
-- (void)immediatelyCallTimer:(NSNumber *)timer;
-
 - (void)enqueueCallback:(NSNumber *)cbID args:(NSArray *)args;
 
 - (void)registerModuleForFrameUpdates:(id<HippyBridgeModule>)module withModuleData:(HippyModuleData *)moduleData;
