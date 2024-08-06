723a051df4044e30171f0782ea4f4a6ca6693281
zealotchen <zealotchen@tencent.com>
2024-08-06 11:57:25 +0800

    feat(dom): revert node sort

--- driver/js/src/modules/scene_builder_module.cc
+++ driver/js/src/modules/scene_builder_module.cc
@@ -435,12 +435,8 @@ std::shared_ptr<ClassTemplate<SceneBuilder>> RegisterSceneBuilder(const std::wea
     if (!scope) {
       return nullptr;
     }
-    auto nodes = HandleJsValue(scope->GetContext(), arguments[0], scope);
-    bool needSortByIndex = false;
-    if (argument_count == 2) {
-       scope->GetContext()->GetValueBoolean(arguments[1], &needSortByIndex);
-    }
-    SceneBuilder::Create(scope->GetDomManager(), scope->GetRootNode(), std::move(std::get<2>(nodes)), needSortByIndex);
+    auto ret = HandleJsValue(scope->GetContext(), arguments[0], scope);
+    SceneBuilder::Create(scope->GetDomManager(), scope->GetRootNode(), std::move(std::get<2>(ret)));
     return nullptr;
   };
   class_template.functions.emplace_back(std::move(create_func_def));
